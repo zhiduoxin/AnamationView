b4e020d7f1ae656620303090655f1cc185163b27
lx <lx@lxdemacbook-pro.local>
2017-03-03 15:26:01 +0800

    Initial Commit

---
+++ Anamationview/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Anamationview
+//
+//  Created by lx on 17/3/3.
+//  Copyright © 2017年 lx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnamationviewTests/AnamationviewTests.m
@@ -0,0 +1,39 @@
+//
+//  AnamationviewTests.m
+//  AnamationviewTests
+//
+//  Created by lx on 17/3/3.
+//  Copyright © 2017年 lx. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AnamationviewTests : XCTestCase
+
+@end
+
+@implementation AnamationviewTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
